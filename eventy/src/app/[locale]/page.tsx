e34b71baef4a3bd29d5255cefa43ab482b499003
Kyrylo Volynskyi <kyrylo@MacBook-Pro-Kyrylo.local>
2025-01-26 09:36:50 +0100

    feat: типизация для евентов в home-page

--- eventy/src/app/[locale]/page.tsx
+++ eventy/src/app/[locale]/page.tsx
@@ -1,3 +1,34 @@
+type Event = {
+  title: string;
+  description: string;
+  tags: string[];
+  price: number;
+  currency: string; //enum?
+  numberOfSeats: number;
+  date: string;
+  time: string;
+  country: string;
+  city: string;
+  backgroundImage: string;
+};
+
+const EVENTS: Event[] = [
+  {
+    title: 'EVENT TITLE',
+    description:
+      'Онлайн-ідеатон з розвитку healthtech стартапів — “Generation H: Ideathon” від SET University',
+    tags: ['#Badges', '#Badges-2'],
+    price: 250,
+    currency: 'UAH',
+    numberOfSeats: 100,
+    date: '2024-10-22',
+    time: '10:10',
+    country: 'Ukraine',
+    city: 'Dnipro',
+    backgroundImage: '/home-page/background-event.png',
+  },
+];
+
 export default function HomePage() {
   return (
     <div>
