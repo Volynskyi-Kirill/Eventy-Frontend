0c438499910d0047eff659a0d1e7f8bfa013dbc2
Volynskyi-Kirill <kirill108bol@gmail.com>
2025-01-30 12:56:31 +0100

    feat: вторая версия карусели, но пока без точек

--- eventy/src/components/home-page/event-hero.tsx
+++ eventy/src/components/home-page/event-hero.tsx
@@ -8,21 +8,41 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { EventCard } from './event-card';
+import { type CarouselApi } from '@/components/ui/carousel';
 
 interface EventHeroProps {
   events: Event[];
 }
 
 export function EventHero({ events }: EventHeroProps) {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
 
   return (
     <section className='absolute top-0 left-0 h-[100dvh] w-full overflow-hidden z-0'>
       <Carousel
         className='w-full h-full'
-        onSelect={(index) => setCurrentIndex(index)}
+        opts={{
+          align: 'start',
+          loop: true,
+        }}
+        setApi={setApi}
       >
         <CarouselContent>
           {events.map((event, index) => (
@@ -34,16 +54,6 @@ export function EventHero({ events }: EventHeroProps) {
         <CarouselPrevious className='absolute left-4 top-1/2 -translate-y-1/2 rounded-full bg-black/20 p-2 text-white backdrop-blur-sm transition-colors hover:bg-black/40' />
         <CarouselNext className='absolute right-4 top-1/2 -translate-y-1/2 rounded-full bg-black/20 p-2 text-white backdrop-blur-sm transition-colors hover:bg-black/40' />
       </Carousel>
-      <div className='absolute bottom-8 left-1/2 flex -translate-x-1/2 gap-2'>
-        {events.map((_, index) => (
-          <button
-            key={index}
-            className={`h-2 w-2 rounded-full ${
-              index === currentIndex ? 'bg-white' : 'bg-white/50'
-            }`}
-          />
-        ))}
-      </div>
     </section>
   );
 }
